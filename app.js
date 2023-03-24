71c54a966e3cac888a107f32bac5db3cf4df2d31
Ahmet Ufuk Kurt <ahmetufuk159@gmail.com>
2023-03-24 22:26:29 +0300

    Geçmiş dersler

---
+++ app.js
@@ -0,0 +1,41 @@
+//? html = hyper text markup language
+//? charset= kullanılan klavye türü
+//? etiketler=>
+//* <p> => paragraf bizim metinlerimizi sakladığım etiket
+//* <h> => h1 den başlayarak h6 ya doğru küçülerek ileryelen başlıklarda veya yan başlıklarda kullandığımız etiket
+//? ctrl+s = yaptığımız değişikleri kaydetmek için kullanılır.
+//? ctrl+z = yaptığımız son değişlikliği siler
+//? ctrl+v = belirlediğimiz yeri yapitırır.
+//? ctrl+c = belirlediğimiz yeri kopyalar.
+//* img=> sayfamızda kullanacağımız png jpg ya da diğer resim
+//* formtlarını ekrana yazdırırken kullandığımız etiketimiz
+//* img etiketi içersine width ile boyutunu ayarlayabiliriz.
+//* property özelliklik anlamına gelir.
+//? DERS3
+//* SATIRLARIN YAN YANA OLMA DURUMU
+//* SADECE VE SADECE KULLANILAN İKİ ELEMENT İNLİNE İSE BU DURUM GERÇEKLEŞİR
+//* div sayfamızı bölerken parçaları içine eklediğimiz bir kutu olarak düşünün.
+//* span sayfamızda ya da buluduğu elementin içerisinden ayırmak için kullandığımız bir inline elementtir.
+
+//* CLASS VE ID SELECTOR (SEÇİCİLER)
+//* 1-) ADLANDIRMA
+//* küçük harfle başlanacak eğer classın ismi 1 den fazla ise arayı tire işareti(-)ile ayrılacak.
+//* rakamla başlanmayacak büyük harf kullanılmayacak
+//* classları seçerken ( .classismi )
+//? id selector
+//! idler unique bir kavramdır sadece bir kere kullanılır ve genellikle id kullanmak kaçınılır.
+
+//? CSS => CASCADİNG STYLE SHEET
+//? CSS NEDİR => HTML İLE OLUŞTURALAN İÇERİĞİ BİÇİMLENDİRME ŞEKLİDİR.
+//? CSS EKLEME METOTDLARI
+//? 1-) İNLİNE CSS : KULLANILMAZ.
+//?  <tag style="özellik"> içerik </tag>
+//? 2-) internal css : bazı durumlarda kullanılır. genellikle terchil edilmez
+//? head etikleri içiersine eklenip style tagları arasından elemente erişip biçimlendirme yapma metotdu
+//? 3-) external css: kullanılan tek gerçektir.
+//? rel kullanılan özellik için sorduğu soru href : dosyanın konumu ve adı
+//? css yazım kuralı
+//*     selector{
+//*      özellikler;
+//*      özellikler;
+//*         }
