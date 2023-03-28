d693759c4e1684b038046bbbb3a27e008e4bf37b
Ahmet Ufuk Kurt <ahmetufuk159@gmail.com>
2023-03-28 23:51:27 +0300

    28.03.2023
    Javascript ilk ders

---
+++ javascript/index.js
@@ -0,0 +1,97 @@
+console.log("AHMET UFUK KURT");
+//? JAVASCRİPT NEDİR
+//* JAVASCRİPT
+//* YÜKSEK LEVEL
+//* OBJE TABANLI
+//* ÇOK PARADİGMALI
+//* PROGRAMLAMA DİLİR.
+
+//* HTML = İSİM İSE
+//* CSS= SIFATI
+//* JS= FİLLDİR
+//* KIRMIZI ARABA GİDİYOR.
+
+//? değişken atama tanımlaa
+//* let değişken ismi = atamak istediğim değişken
+//? değişken isimleri ayarlamak
+//* camelCase kullanılır.
+//* değişkene uygun bir isim seçilir
+
+//* veri tipleri
+//* 1 obje
+//* primitive(ilkel)
+
+//* primitive(ilkel) değişkenler
+//* 1-) string => metinler için kullanılır arada bir sayılar için
+//* 2-) number=> sayılar için kullanılır
+//* 3-) boolean=> true ve false değerlerini alır => karar verirken kullanılır
+//* 4-)undefined=> değer almamış boş değişkenler
+//* 5-) null=> boş anlamına
+//* 6-) symbol=> eski bir değişkendir kullanmayız
+//* 7-) bigIng=> 10üzeri 15 gibi büyük sayıalr için kullanılır.
+
+//* string
+//* stringler tanımlarınken tırnak içine yazılır.
+let mySchool = "Yalova Üniversitesi";
+//* değişkenleri tırnassık bir şekilde console içine eklenerek değişkene atadığımız veriyi consolda inceleebilriz
+console.log(mySchool);
+
+//* number
+//* numberları tanımlarken tırnak kullanma ihtiyacımız yoktur.
+//* eğer tırnak kullanılırsak yazdığımız değişken
+//* sayı bile olsa sanki bir stringmiş gibi kullanılır
+
+let age = 21;
+let myFatherAge = "50";
+console.log(age);
+console.log(myFatherAge);
+
+let showPassword = true;
+let hidePassword = false;
+
+//? AND OPERATORÜ   &
+//* VE ANLAMINA İKİ KOŞULUNDA SAĞLMASIN DURUMNDA ÇALIŞIR
+
+//* A DURUMU B DURUMU
+//* DURUMLARIN İKİSİDE SAĞLANIRSA BANA TRUE VERİR
+
+//? OR OPERATÖRÜ ||
+
+let person = "eslem";
+let person2 = "karin";
+
+//* A DURUMU B DURUMU
+//* İKİ DURUMDAN İKİSİNDEN BİR GERÇEKLEŞTİĞİ TRUE DÖNER
+
+//? NOT OPERATÖRÜ !
+let ufukVision = true;
+let ufukHasDrivingLicence = true;
+
+//? MATEMATİK OPERATÖRLER
+//* ,>, < ,>= ,<= ,   eşitlik =>  == , ===
+let myAge = "19";
+console.log(myAge === 19);
+let myBudge = 3200;
+//* javascripte bir değişken başka bir değişken ya da değer eşit mi kontrol ederken iki = kullanılır (==)
+//! not : bir eşittir atama için kullanılır.
+
+//*let const
+//* let değiştirebilien değerler
+//* const ise değişmez değerler için
+const myCounter = "turkey";
+
+//*
+//* ülkenizi tanımayın konsola yazdırın.
+//* ülkenizin konuştuğu dil
+//* isim soy isim yaş meslek
+//* ülkenizin nüfusu
+//* ülkeniz adamı
+//* ülkeniz hangi kıtada
+//* konsolda yazılacak
+
+console.log(myAge > 18);
+//* yukarıda ki örnek gibi bütün operatörleri ve değişkenleri kullanarak true false çıktısı
+const myCountry = "turkey";
+//* artı olarak yukarıda verdiğim & || ve ! operatörünü kullanmaya çalışın
+//* yaşı 15 olup 4000 tlsi olan biri yaşının 18 i geçip parasın en az 3000 tl olması gerken bir ülkede ehliyet alabilir mi
+//* ufuğun gözleri iyi görmüyor ama ehliyeti var akşam araba sürebilir mi ?
