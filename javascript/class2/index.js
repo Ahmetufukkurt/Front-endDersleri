692d650c30fdd916859457efa46ecbedc5630375
Ahmet Ufuk Kurt <ahmetufuk159@gmail.com>
2023-03-31 18:36:57 +0300

    31.03.2023

---
+++ javascript/class2/index.js
@@ -0,0 +1,53 @@
+//? JAVASCRİPT YÜKSEK SEVİYELİ
+//? ÇOK PARADİGMALI
+//? OBJE TABANLI
+//? PROGRAMLAMA DİLİDİR
+
+console.log("Hello World!");
+
+//? VERİ TİPLERİ İKİYE AYRILIR
+//? BİRİNCİ OBJE TABANLI
+//? PRİMİTİVE YANİ İLKEL
+
+//? İLKEL VERİLER
+//*Number==> sayıları tanımlarken kullanılırz.
+//*String ==> string bizim textlerimizdir yazılarımız
+//*Boolean ==> true or false değeri alır karar verirken kullanılırız
+
+//*undefined ==> tanımlanmamış (boş anlamına gelir)
+//*null ==> boş demek
+//*symbol es2015 ==> symbol eski bir veri tipidir kullanmayız sadece bilgi için.
+//*bigInt ==> buda 10 üzeri 10 gibi 10 üzeri 15 gibi büyük sayılar için kullandığımız.
+
+//? number veri tipi
+//* değişkeni atadıktan sonra tırnak kullanılmaz
+
+let age = 21;
+console.log(age);
+
+//? String veri tipi
+//* değişkene atarken "tırnak içersinde ifade edilir"
+
+const myName = "Ahmet Ufuk";
+console.log(myName);
+
+//! console.log(içersiine değişken atarken sadece değişken ismi yazılır)
+//! Değişken ismi verirken camelCase Kullanılır.
+
+//? camelCase=birden fazla kelime içeren değişkenleri
+//? ikinci kelimesinden itibaren büyük harfle yazdığımız
+//? bir adlanlandırma şeklidir
+
+const myFullName = "Ahmet ufuk kurt";
+console.log(myFullName);
+//? Const ve let , var
+
+const myCountry = "Adana";
+let myJob = "student";
+
+console.log(myCountry);
+console.log(myJob);
+console.log(myCountry, myJob);
+
+console.log(typeof "ahmet");
+console.log(23);
